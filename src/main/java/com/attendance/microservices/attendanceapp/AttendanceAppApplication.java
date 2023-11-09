981490aaad29ca2e3e73c05a93205f9abb0f78b1
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-11-09 20:40:22 +0530

    Initial Commit

---
+++ src/main/java/com/attendance/microservices/attendanceapp/AttendanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.microservices.attendanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceAppApplication.class, args);
+	}
+
+}
