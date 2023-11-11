366d68112107cc1509b0de68e064f1e82f7b0a15
Anurag Sahu <anishsahu33@gmail.com>
2023-11-11 22:26:36 +0530

    gitignore not working

---
+++ src/test/java/com/attendance/microservices/attendanceapp/AttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendance.microservices.attendanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
