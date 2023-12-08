6545e76b3d450756beeed90c01fd090727cc2813
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-12-08 17:34:28 +0530

    Re-add Files

---
+++ src/main/java/com/attendance/microservices/attendanceapp/entities/Attendance.java
@@ -0,0 +1,23 @@
+// package com.attendance.microservices.attendanceapp.entities;
+
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// @Data
+// @Builder
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Entity
+// @Table(name = "attendance")
+// public class Attendance {
+
+//     private String rollNo;
+//     private int subjectId;
+//     private String date;
+//     private boolean present;
+
+// }
