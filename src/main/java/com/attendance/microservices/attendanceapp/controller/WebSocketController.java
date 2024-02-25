5cfb54b50e107235e261da673aed99955e8026ae
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2024-02-25 19:47:42 +0530

    socket config

--- src/main/java/com/attendance/microservices/attendanceapp/controller/WebSocketController.java
+++ src/main/java/com/attendance/microservices/attendanceapp/controller/WebSocketController.java
@@ -6,27 +6,12 @@ import org.springframework.stereotype.Controller;
 
 import com.attendance.microservices.attendanceapp.dto.IotSignalDTO;
 
-
 @Controller
 public class WebSocketController {
-    // @Autowired
-    // SimpMessagingTemplate template;
 
-    // @PostMapping("/api/v1/testSignal")
-    // public ResponseEntity<Void> testSignal(@RequestBody textMsgDTO textMesgDTO) {
-    //     // String signal = "ON";
-    //     template.convertAndSend("/topic/signal", textMesgDTO);
-    //     System.out.println("Sent: " + textMesgDTO);
-        
-    //     return new ResponseEntity<>(HttpStatus.OK);
-    // }
-    
-
-    // @MessageMapping("/device.sendSignal")
     @SendTo("/topic/signal")
     public IotSignalDTO sendSignal(@Payload IotSignalDTO signalDTO) {
         return signalDTO;
     }
-
 }
 
